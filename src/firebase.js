b0784808fc3a5ab5cacad9d2beb1d9eeaf03e4c1
Jong jae Park <pjj186@naver.com>
2021-09-21 01:09:14 +0900

    파이어베이스 셋업

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLyRNqW5KP1M4caQq3aqwpzJz0rEK2PDk",
+  authDomain: "tweet-449b7.firebaseapp.com",
+  projectId: "tweet-449b7",
+  storageBucket: "tweet-449b7.appspot.com",
+  messagingSenderId: "650140040425",
+  appId: "1:650140040425:web:1048b977b71f25867e4478",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
