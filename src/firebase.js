f7018d50ba0aa08166ff13907fa29ec663efa48f
Jong jae Park <pjj186@naver.com>
2021-09-21 16:50:27 +0900

    .env
    파이어베이스 콘픽에 관련된 값들을 환경변수로 만들어줌

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLyRNqW5KP1M4caQq3aqwpzJz0rEK2PDk",
-  authDomain: "tweet-449b7.firebaseapp.com",
-  projectId: "tweet-449b7",
-  storageBucket: "tweet-449b7.appspot.com",
-  messagingSenderId: "650140040425",
-  appId: "1:650140040425:web:1048b977b71f25867e4478",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
