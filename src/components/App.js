bfa2955dc516556286a6635524f7d6a15d24f779
Jong jae Park <pjj186@naver.com>
2021-09-23 02:30:21 +0900

    파이어베이스 Auth 셋업 / 로그인 폼 만들기

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Router from "./Router";
+import { authService } from "../fbase";
 
 function App() {
-  return <Router />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return <Router isLoggedIn={isLoggedIn} />;
 }
 
 export default App;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
-const Router = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const Router = ({ isLoggedIn }) => {
   return (
     <BrowserRouter>
       <Switch>
         {isLoggedIn ? (
           <>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
             </Route>
           </>
