43f1b65df148920d896fb4a928416ead91c2d53a
Jong jae Park <pjj186@naver.com>
2021-10-26 09:50:01 +0900

    Bugfix

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,22 @@ function App() {
   useEffect(() => {
     // 사용자의 로그인 상태의 변화를 관찰하는 관찰자를 추가시켜줌
     authService.onAuthStateChanged((user) => {
+      console.log(user);
       if (user) {
-        setUserObject({
-          displayName: user.displayName,
-          uid: user.uid,
-          // 여기서 updateProfile 함수는 우리가 원하는 updateProfile 함수를 제공해주는 역할
-          updateProfile: (args) => user.updateProfile(args),
-        });
-      } else {
+        if (user.displayName === null) {
+          setUserObject({
+            displayName: "User",
+            uid: user.uid,
+            updateProfile: (args) => user.updateProfile(args),
+          });
+        } else {
+          setUserObject({
+            displayName: user.displayName,
+            uid: user.uid,
+            // 여기서 updateProfile 함수는 우리가 원하는 updateProfile 함수를 제공해주는 역할
+            updateProfile: (args) => user.updateProfile(args),
+          });
+        }
       }
       setInit(true);
     });
